5701e52e13a9bf85d8ccabcefc736d75dbdf17df
RileyManda <rileymanda69@gmail.com>
2023-02-10 18:56:17 +0200

    footer component

---
+++ components/Footer.jsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import { Stack } from '@mui/system';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://cixhub.com">
+        CixHub
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+// todo add social icons|footer
+const footer = {
+title: 'cixhub',
+social: [
+    { name: 'GitHub', icon: GitHubIcon },
+    { name: 'Twitter', icon: TwitterIcon },
+    { name: 'Facebook', icon: FacebookIcon },
+  ],
+
+}
+export default function Footer() {
+  return (
+    <Box component="footer" sx={{ bgcolor: 'background.paper', py: 6 }}>
+      <Container maxWidth="lg">
+        <Copyright />
+      </Container>
+    </Box>
+  );
+}
